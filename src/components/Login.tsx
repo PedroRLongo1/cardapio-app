741b76b35ee0b67a5bd974de69d6a42214bf3794
PedroRLongo1 <pedrorlongo@gmail.com>
2025-07-24 21:10:56 -0400

    Login pt 2

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,74 +1,86 @@
 import React from "react";
-import '../estilos/Login.css';
+import "../estilos/Login.css";
 import { useNavigate } from "react-router-dom";
 import useForm from "../hooks/useForm";
+import { Input } from "./Input";
+import Container from "./Container";
+import api from "../services/api";
 
 interface SnackbarState {
-    message: string;
-    duration: number;
-    type?: 'success' | 'error' | 'warning' | 'info';
+  message: string;
+  duration: number;
+  type?: "success" | "error" | "warning" | "info";
 }
 
 const Login: React.FC = () => {
-    const { values, errors, handleChange, validate } = useForm({
-        email: "",
-        senha: "",
-    });
+  const { values, errors, handleChange, validate } = useForm({
+    email: "",
+    senha: "",
+  });
 
-    const [ snackbar, setSnackbar ] = React.useState<SnackbarState>({
-        message: "",
-        type: 'success',
-        duration: 0,
-    });
+  const [snackbar, setSnackbar] = React.useState<SnackbarState>({
+    message: "",
+    type: "success",
+    duration: 0,
+  });
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className="background">
-            <div className="login-card">
-                <h2>
-                    Login
-                </h2>
-                <div  className="data-input">
-                    <label>Email</label>
-                    <input
-                        type="text"
-                        value={values.email}
-                        onChange={handleChange("email")}
-                        className={errors.email
-                            ? "input-error"
-                            : values.email
-                            ? "input-success"
-                            : "input-default"
-                        }
-                    />
-                    {errors.email && <p>{errors.email}</p>}
-                </div>
-                <div className="data-input">
-                    <label>Senha</label>
-                    <input
-                        type="text"
-                        value={values.senha}
-                        onChange={handleChange("senha")}
-                        className={errors.senha
-                            ? "input-error"
-                            : values.senha
-                            ? "input-success"
-                            : "input-default"
-                        }
-                    />
-                    {errors.senha && <p>{errors.senha}</p>}
-                </div>
-                <button
-                    className="login-button"
-                    type="button"
-                    onClick={() => {}}
-                >
-                    Entrar
-                </button>
-            </div>
-        </div>
-    )
-}
+  const login = async () => {
+    const duration = 3000; // 3 seconds
+
+    if (!validate) {
+      return;
+    }
+
+    try {
+      const response = await api.post<{
+        token: string;
+        refreshToken: string;
+        message: string;
+      }> ("/login", {
+        email: values.email,
+        senha: values.senha,
+      });
+
+      const { token, refreshToken, message } = response.data;
+
+      localStorage.setItem("token", token);
+      localStorage.setItem("refreshToken", refreshToken);
+      setSnackbar({
+        message: message || "Login successful!",
+        duration,
+        type: "success",
+      });
+      setTimeout(() => {
+        navigate("/");
+      },duration);
+  } catch (error:unknown) {}
+
+  return (
+    <Container>
+      <h2>Login</h2>
+      <div className="data-input">
+        <label>Email</label>
+        <Input
+          value={values.email}
+          type="text"
+          onChange={handleChange("email")}
+        />
+      </div>
+      <div className="data-input">
+        <label>Senha</label>
+        <Input
+          value={values.senha}
+          type="text"
+          onChange={handleChange("senha")}
+        />
+      </div>
+      <button className="login-button" type="button" onClick={() => {}}>
+        Entrar
+      </button>
+    </Container>
+  );
+};
 
 export default Login;
